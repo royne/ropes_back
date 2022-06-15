834a63a8a5c59e0ad861ca226f4b8d01ef7c20f2
royne <royne.chaves@gmail.com>
2022-06-14 23:46:54 -0500

    fix typo

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -12,7 +12,7 @@ class CategorySerializer < ActiveModel::Serializer
       {
         id: x.id,
         name: x.name,
-        image_url: Cloudinary::Utils.cloudinary_url(x.photos.first.key, cloud_name: ENV['cloudinary_cloud_name'] )
+        image_url: Cloudinary::Utils.cloudinary_url(x.photos.first.key, cloud_name: ENV['cloudinary_cloud_name'] ),
         # image_url: url_for(x.photos.last),
         public_url: x.public_url
       }
